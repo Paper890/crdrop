9a3ed65d589f2d6d1a032b6499d47b81f81696b8
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-05 11:09:32 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install python3 python3-pip
+apt install python3-pip -y
+pip install requests
+pip install python-telegram-bot==12.0.0
+pip install schedule
