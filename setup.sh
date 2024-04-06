af23cc7ac8db3cfc0020489c4a5e3bf723aabdbe
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-06 23:02:42 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo -e "ISI TOKEN GITHUB UNTUK REGIS IP"
 read -p "Masukkan Token DO :" token_do
 read -p "Masukkan Token Telegram : " token_tele
+read -p "Masukkan Token Github : " token_git
 # Update paket repository
 #sudo apt update -y
 
@@ -22,8 +24,14 @@ read -p "Masukkan Token Telegram : " token_tele
 # Mengunduh skrip Python
 wget https://raw.githubusercontent.com/Paper890/crdrop/main/Do.py
 
-# Ganti nilai teks dalam skrip Python
+# FOR DO CREATE
 TOKEN_DO="$token_do"
+sed -i "s/{TOKEN_DO}/$TOKEN_DO/g" Do.py
+TOKEN_TELEGRAM="$token_tele"
+sed -i "s/{TOKEN_TELEGRAM}/$TOKEN_TELEGRAM/g" Do.py
+
+# FOR REGIS IP
+TOKEN_GITHUB="$token_git"
 sed -i "s/{TOKEN_DO}/$TOKEN_DO/g" Do.py
 TOKEN_TELEGRAM="$token_tele"
 sed -i "s/{TOKEN_TELEGRAM}/$TOKEN_TELEGRAM/g" Do.py
