c87c740cac067ee7315c475d2e21db9987e4dd4a
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-06 15:33:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install python3 python3-pip
+sudo apt install python3 python3-pip -y
 apt install python3-pip -y
 pip install requests
 pip install python-telegram-bot==12.0.0
 pip install schedule
+
+mkdir -p san/bot
+cd
+cd san/bot
