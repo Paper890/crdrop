8ba184aef6b2fcdd29f3326d06ae57a97f8b867a
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-07 07:23:31 +0800

    Create setup1.sh

---
+++ setup1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo -e "ISI TOKEN GITHUB UNTUK REGIS IP"
+read -p "Masukkan Token DO :" token_do
+read -p "Masukkan Token Telegram : " token_tele
+read -p "Masukkan Token Github : " token_git
+
+#Set LocalTime
+sudo timedatectl set-timezone Asia/Jakarta
+
+
+mkdir -p san/script/bot
+# Pindah ke dalam folder yang baru dibuat
+cd san/script/bot
+# Mengunduh skrip Python
+wget https://raw.githubusercontent.com/Paper890/crdrop/main/Do.py
+wget https://raw.githubusercontent.com/Paper890/crdrop/main/regis.py
+
+# FOR DO CREATE
+TOKEN_DO="$token_do"
+sed -i "s/{TOKEN_DO}/$TOKEN_DO/g" Do.py
+TOKEN_TELEGRAM="$token_tele"
+sed -i "s/{TOKEN_TELEGRAM}/$TOKEN_TELEGRAM/g" Do.py
+
+# FOR REGIS IP
+TOKEN_GITHUB="$token_git"
+sed -i "s/{TOKEN_GITHUB}/$TOKEN_GITHUB/g" Do.py
+TOKEN_TELEGRAM="$token_tele"
+sed -i "s/{TOKEN_TELEGRAM}/$TOKEN_TELEGRAM/g" Do.py
+
+# fungsi running as system
+cd
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/Paper890/crdrop/main/Do.service
+sudo systemctl daemon-reload
+sudo systemctl start Do
+sudo systemctl enable Do
+sudo systemctl restart Do
+
+wget https://raw.githubusercontent.com/Paper890/crdrop/main/regis.service
+sudo systemctl daemon-reload
+sudo systemctl start regis
+sudo systemctl enable regis
+sudo systemctl restart regis
+
+echo "Instalasi selesai."
