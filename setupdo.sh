3d5a962ccfbefc5d373c6607a42120d978692163
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-11 06:50:38 +0800

    Create setupdo.sh

---
+++ setupdo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "UNTUK CREATE DROPLET"
+read -p "Masukkan Token DO :" token_do
+read -p "Masukkan Token Telegram : " token_tele
+sleep 2
+
+wget https://raw.githubusercontent.com/Paper890/crdrop/main/Do.py
+
+# FOR DO CREATE
+TOKEN_DO="$token_do"
+sed -i "s/{TOKEN_DO}/$TOKEN_DO/g" Do.py
+TOKEN_TELEGRAM="$token_tele"
+sed -i "s/{TOKEN_TELEGRAM}/$TOKEN_TELEGRAM/g" Do.py
+
+echo "Instalasi selesai."
